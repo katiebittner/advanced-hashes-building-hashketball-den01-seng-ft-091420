c2fb5fe14120a4f087af94eee52cb8307e75744c
Katie B <katiebittner1@gmail.com>
2020-09-08 22:02:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,8 @@
 def game_hash
   hash = {
     :home => {
-      :team_name => "",
-      :colors => [],
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black","White"],
       :players => [{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},]
     },
     :away => {
