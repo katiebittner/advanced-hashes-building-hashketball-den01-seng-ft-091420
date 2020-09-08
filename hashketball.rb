56d2b9b2e959adaae77e0a56e29129835369ef2c
Katie B <katiebittner1@gmail.com>
2020-09-08 21:58:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,15 @@
 # Write your code here!
+
+
+def game_hash
+  hash = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {}
+        ]
+    },
+    :away => {}
+  }
+end
