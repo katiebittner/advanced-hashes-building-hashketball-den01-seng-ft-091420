bb2a3c2539df9116be9dccf2eaa63b47e5d242d7
Katie B <katiebittner1@gmail.com>
2020-09-08 22:02:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,16 +6,12 @@ def game_hash
     :home => {
       :team_name => "",
       :colors => [],
-      :players => [{:player_name
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks},{},{},{},{}]
+      :players => [{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},]
     },
-    :away => {}
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => [{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},{:player_name =>,:number =>,:shoe =>,:points =>,:rebounds =>,:assists =>,:steals =>,:blocks =>,:slam_dunks =>},]
+    }
   }
 end
